7cd3c4db3fad69d6c7cfaaef8be9239cd9363b19
Namoral Thy <thynamoral18@gmail.com>
2025-05-14 11:13:51 +0700

    migrate to postgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2025_05_13_093338) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -22,7 +25,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_13_093338) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
